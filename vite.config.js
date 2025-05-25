e7f570b90132b7bb45e6521e2725ca31845f4f2e
Олена <olena@example.com>
2025-05-25 16:40:52 +0300

    add markup header

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  root: 'public',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
+  plugins: [react()],
+  base: "./"
 })
