e0b9195741ee9a130a13c609f4219e3468a66590
Олена <olena@example.com>
2025-05-25 17:54:09 +0300

    add markup header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Description from "./components/Description/Description";
-import Options from "./components/Options/Options";
+import Options from "./components/Options/Options";gg
 import Feedback from "./components/Feedback/Feedback";
 import Notification from "./components/Notification/Notification";
 
