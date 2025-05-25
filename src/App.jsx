e61c2c54b880fe5ce90f71bc3cf74a76b0e111bc
Олена <olena@example.com>
2025-05-25 17:37:44 +0300

    add markup header

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,64 @@
-import css from "./Options.module.css"; // Якщо є стилі
+import { useEffect, useState } from "react";
+import Description from "./components/Description/Description";
+import Options from "./components/Options/Options";
+import Feedback from "./components/Feedback/Feedback";
+import Notification from "./components/Notification/Notification";
 
-export default function Options({ options, updateFunction, totalFeedback }) {
+function App() {
+  let startData = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  const [state, setState] = useState(() => {
+    const savedStats = JSON.parse(window.localStorage.getItem("stats"));
+    if (savedStats) {
+      return savedStats;
+    }
+    return startData;
+  });
+
+  const totalFeedback = state.good + state.neutral + state.bad;
+  const positive = Math.round((state.good / totalFeedback) * 100);
+
+  const updateFeedback = (feedbackType) => {
+    if (feedbackType === "reset") {
+      setState(startData);
+      return;
+    }
+    setState((prevState) => ({
+      ...prevState,
+      [feedbackType]: prevState[feedbackType] + 1,
+    }));
+  };
+
+  useEffect(() => {
+    window.localStorage.setItem("stats", JSON.stringify(state));
+  }, [state]);
+
   return (
-    <div className={css.btnWrapper}>
-      {options.map((option) => (
-        <button
-          key={option}
-          onClick={() => updateFunction(option)}
-          className={css.btn}
-        >
-          {option}
-        </button>
-      ))}
-      {totalFeedback > 0 && (
-        <button onClick={() => updateFunction("reset")} className={css.btn}>
-          Reset
-        </button>
+    <>
+      <Description
+        title="Sip Happens Café"
+        text="Please leave your feedback about our service by selecting one of the options below."
+      />
+      <Options
+        options={Object.keys(state)}
+        updateFunction={updateFeedback}
+        totalFeedback={totalFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          feedback={state}
+          totalFeedback={totalFeedback}
+          positive={positive}
+        />
+      ) : (
+        <Notification />
       )}
-    </div>
+    </>
   );
 }
+
+export default App;
